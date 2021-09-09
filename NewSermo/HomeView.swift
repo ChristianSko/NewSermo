c67925b26e0c32dd43054d3218d65725f7efb216
Christian Skorobogatow <christiansko@icloud.com>
2021-09-09 17:54:15 +0200

    feat(): add assets from Sermo 1.0

--- NewSermo/HomeView.swift
+++ NewSermo/HomeView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,8 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            HomeView()
+        }
     }
 }

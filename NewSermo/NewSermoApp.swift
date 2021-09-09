76d01d6670c35fd01df667713772eb35dab5d1e8
Christian Skorobogatow <christiansko@icloud.com>
2021-09-09 17:33:49 +0200

    Initial Commit

---
+++ NewSermo/NewSermoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSermoApp.swift
+//  NewSermo
+//
+//  Created by Christian Skorobogatow on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct NewSermoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

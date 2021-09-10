8d264720cb05570bd8046c5c42013a6a78ef3509
Christian Skorobogatow <christiansko@icloud.com>
2021-09-10 10:37:52 +0200

    feat(): add constant utilities files with ImageAsset reference

---
+++ NewSermo/Utilities/Extensions/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NewSermo
+//
+//  Created by Christian Skorobogatow on 10/9/21.
+//
+
+import Foundation
+
+enum ImageAsset {
+    static let homeBackground = "background-menu"
+}

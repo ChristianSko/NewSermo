418d9828823acde6cb084df5640294436c2652ea
Christian Skorobogatow <christiansko@icloud.com>
2021-09-16 23:19:25 +0200

    test(): core haptics implementation

--- NewSermo/Screens/FlashcardView.swift
+++ NewSermo/Screens/FlashcardView.swift
@@ -6,10 +6,14 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct FlashcardView: View {
     
     let color: Color
+    
+    
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
         ZStack{
@@ -33,6 +37,8 @@ struct FlashcardView: View {
                                color: color) {
                         print("Haptic Feedback on here")
                     }
+                    .onAppear(perform: prepareHaptics)
+                    .onTapGesture(perform: complexSuccess)
                         
                     HStack(spacing: 70){
                         
@@ -56,6 +62,42 @@ struct FlashcardView: View {
         }
         
     }
+    
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+
+    
 }
 
 struct Fashcard_Previews: PreviewProvider {

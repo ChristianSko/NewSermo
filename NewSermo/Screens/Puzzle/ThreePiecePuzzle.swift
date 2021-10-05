49b77b71b3a8a0c6636782c2a4b19ed8dbfe5203
Christian Skorobogatow <christiansko@icloud.com>
2021-10-05 22:29:50 +0200

    feat(): add stars 3 puzzle

--- NewSermo/Screens/Puzzle/ThreePiecePuzzle.swift
+++ NewSermo/Screens/Puzzle/ThreePiecePuzzle.swift
@@ -18,88 +18,112 @@ struct ThreePiecePuzzle: View {
     var body: some View {
         ZStack(alignment: .bottomLeading){
             ZStack(alignment: .bottomTrailing){
-                ZStack{
-                    BackgroundView(imageName: "backgroundpuzzle-market" )
-                    
-                    
-                    HStack(spacing: 30) {
+                ZStack {
+                    ZStack{
+                        BackgroundView(imageName: "backgroundpuzzle-market" )
                         
                         
-                        
-                        
-                        VStack {
-                            MaskedPuzzleImage(imageName: "apple",
-                                              color: Color.blue,
-                                              cellSize: 150,
-                                              cornerRadius: 10,
-                                              borderWidth: 10,
-                                              maskImageName: "puzzle-3pc-2")
-                                .scaleEffect(topLeadingPuzzleAnimation ? 2 : 1)
-                                .offset(x: topLeadingPuzzleAnimation ? 260 : 0 ,
-                                        y: topLeadingPuzzleAnimation ? 20 : 0)
-                                .onTapGesture {
-                                    withAnimation {
-                                        topLeadingPuzzleAnimation.toggle()
-                                        if trailingPuzzleAnimation && bottomleadingPuzzleAnimation && topLeadingPuzzleAnimation {
-                                            withAnimation {
-                                                showStars.toggle()
+                        HStack(spacing: 30) {
+                            
+                            
+                            
+                            
+                            VStack {
+                                MaskedPuzzleImage(imageName: "apple",
+                                                  color: Color.blue,
+                                                  cellSize: 150,
+                                                  cornerRadius: 10,
+                                                  borderWidth: 10,
+                                                  maskImageName: "puzzle-3pc-2")
+                                    .scaleEffect(topLeadingPuzzleAnimation ? 2 : 1)
+                                    .offset(x: topLeadingPuzzleAnimation ? 260 : 0 ,
+                                            y: topLeadingPuzzleAnimation ? 20 : 0)
+                                    .onTapGesture {
+                                        withAnimation {
+                                            topLeadingPuzzleAnimation.toggle()
+                                            if trailingPuzzleAnimation && bottomleadingPuzzleAnimation && topLeadingPuzzleAnimation {
+                                                withAnimation {
+                                                    showStars.toggle()
+                                                }
                                             }
                                         }
                                     }
-                            }
-                            
-                            
-                            MaskedPuzzleImage(imageName: "apple",
-                                              color: Color.blue,
-                                              cellSize: 150,
-                                              cornerRadius: 10,
-                                              borderWidth: 10,
-                                              maskImageName: "puzzle-3pc-3")
-                                .scaleEffect(bottomleadingPuzzleAnimation ? 2 : 1)
-                                .offset(x: bottomleadingPuzzleAnimation ? 260 : 0 ,
-                                        y: bottomleadingPuzzleAnimation ? -140 : -65)
-                            
-                                .onTapGesture {
-                                    withAnimation {
-                                        bottomleadingPuzzleAnimation.toggle()
-                                        if trailingPuzzleAnimation && bottomleadingPuzzleAnimation {
-                                            withAnimation {
-                                                showStars.toggle()
+                                
+                                
+                                MaskedPuzzleImage(imageName: "apple",
+                                                  color: Color.blue,
+                                                  cellSize: 150,
+                                                  cornerRadius: 10,
+                                                  borderWidth: 10,
+                                                  maskImageName: "puzzle-3pc-3")
+                                    .scaleEffect(bottomleadingPuzzleAnimation ? 2 : 1)
+                                    .offset(x: bottomleadingPuzzleAnimation ? 260 : 0 ,
+                                            y: bottomleadingPuzzleAnimation ? -140 : -65)
+                                
+                                    .onTapGesture {
+                                        withAnimation {
+                                            bottomleadingPuzzleAnimation.toggle()
+                                            if topLeadingPuzzleAnimation && bottomleadingPuzzleAnimation && topLeadingPuzzleAnimation {
+                                                withAnimation {
+                                                    showStars.toggle()
+                                                }
                                             }
                                         }
                                     }
+                                
                             }
+                            .offset(y: 50)
                             
-                        }
-                        .offset(y: 50)
-                        
-
-                            CellView(imageName: "apple",
-                                     color: Color.blue,
-                                     cellSize: 300,
-                                     cornerRadius: 10,
-                                     borderWidth: 10)
-                                .opacity(0.1)
+                            
+                            Spacer()
+                                .frame(width: 300, height: 300)
                                 .offset(y: -30)
                             
-                        
-                        
-                        
-                        MaskedPuzzleImage(imageName: "apple",
-                                          color: Color.blue,
-                                          cellSize: 150,
-                                          cornerRadius: 10,
-                                          borderWidth: 10,
-                                          maskImageName: "puzzle-3pc-1")
-                            .scaleEffect(trailingPuzzleAnimation ? 2 : 1)
-                            .offset(x: trailingPuzzleAnimation ? -250 : 0 ,
-                                    y: trailingPuzzleAnimation ? -10 : 0)
-                        
-                            .onTapGesture {
-                                withAnimation {
-                                    trailingPuzzleAnimation.toggle()
+                            
+                            
+                            
+                            
+                            MaskedPuzzleImage(imageName: "apple",
+                                              color: Color.blue,
+                                              cellSize: 150,
+                                              cornerRadius: 10,
+                                              borderWidth: 10,
+                                              maskImageName: "puzzle-3pc-1")
+                                .scaleEffect(trailingPuzzleAnimation ? 2 : 1)
+                                .offset(x: trailingPuzzleAnimation ? -250 : 0 ,
+                                        y: trailingPuzzleAnimation ? -10 : 0)
+                            
+                                .onTapGesture {
+                                    withAnimation {
+                                        trailingPuzzleAnimation.toggle()
+                                        if topLeadingPuzzleAnimation && bottomleadingPuzzleAnimation && topLeadingPuzzleAnimation {
+                                            withAnimation {
+                                                showStars.toggle()
+                                            }
+                                        }
+                                    }
                                 }
-                            }
+                        }
+                        
+                    }
+                    
+                    CellView(imageName: "apple",
+                             color: Color.blue,
+                             cellSize: 300,
+                             cornerRadius: 10,
+                             borderWidth: 10)
+                        .opacity(0.1)
+                        .offset(y: -30)
+                    
+                    if showStars {
+                        Image("star-puzzle-black")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 370, height: 300)
+                        
+                        Text("Apple")
+                            .font(.system(size: 48))
+                            .fontWeight(.heavy)
                     }
                     
                 }
@@ -110,6 +134,7 @@ struct ThreePiecePuzzle: View {
                 bottomleadingPuzzleAnimation = false
                 trailingPuzzleAnimation = false
                 topLeadingPuzzleAnimation = false
+                showStars = false
             })
         }
     }
